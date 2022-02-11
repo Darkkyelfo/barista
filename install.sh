f436d139a84d4e8208e1f156d30af97618f79497
raulsousa <raul.sousa@mv.com.br>
2022-02-10 23:37:59 -0300

    correção scritp instalação

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "conf:
     bucket: <BUCKET_NAME>
     #Remove the comments bellow if you want to use a different key than the local one
 #    access_key_id: <ACESS_KEY>
-#    secret_access_key: <SECRET_KEY>> conf.yaml"
+#    secret_access_key: <SECRET_KEY" >> conf.yaml
 
 echo "DOWNLOADING PYTHON DEPENDECIES..."
 pip install -r requirements
